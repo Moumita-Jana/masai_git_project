f43d4ddf7068f9fe204725ee21c8f0f8cc85eacf
Moumita-Jana <moumitajana1004@gmail.com>
2022-11-30 19:54:40 +0530

    check or recorrect the code

---
+++ login.js
@@ -0,0 +1,50 @@
+//check Palindrome
+
+let str="naman"
+let bag="";
+
+for(let i=str.length;i<=0;i++){
+   
+  bag+=str[i];
+
+  }
+if(bag==str){
+  console.log("Yes);
+}else{
+  console.log("No");
+}
+
+
+
+
+
+//prime problem
+
+
+let factor=0;
+
+let num=13;
+
+for(let i=1;i<=num;i++){
+  
+  
+if(num%i==0){
+  factor++;
+}
+
+
+if(facctor==1){
+   
+  console.log("Prime");
+}else{
+   console.log("Not Prime){
+ }
+
+
+
+
+
+
+
+
+
